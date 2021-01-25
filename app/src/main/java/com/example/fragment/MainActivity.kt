163579c58e7fb059ed3644dae140415bfa849172
YehJiaJun <a159663@hotmail.com>
2021-01-25 15:19:34 +0800

    up

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        val viewPage = findViewById<ViewPager>(R.id.viewPage)
+        viewPage.adapter = adapter;
     }
+}
+
+class ViewPagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm){
+    override fun getItem(position: Int) = when(position){
+        0->FirstFragment()
+        1->SecondFragment()
+        else->ThirdFragment()
+    }
+    override fun getCount() = 3
 }
---
+++ app/src/main/java/com/example/fragment/SecondFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import java.lang.Appendable
+
+class SecondFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.second,container,false )
+    }
+}
